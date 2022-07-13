b8c4b8d3345056704d93aee650b755d5ccb47fe4
SHAHYASHNILESH <91772554+SHAHYASHNILESH@users.noreply.github.com>
2022-07-13 23:22:21 +0530

    Update Overlay.js

--- website/src/components/Overlay.js
+++ website/src/components/Overlay.js
@@ -3,7 +3,7 @@ import Card from "react-bootstrap/Card";
 
 const Overlay = () => {
   return (
-    <Card className="bg-dark text-primary fw-bold">
+    <Card className="bg-dark text-primary fw-bold mt-3">
       <Card.Img src="https://d35w6hwqhdq0in.cloudfront.net/transform/scale,h=600,w=600,upscale=True:compress/d0c6466f90f458968d90f21d9962d24b.jpg" alt="Card image" height="600"/>
       <Card.ImgOverlay>
         <Card.Title className="display-3 fw-bold mt-5" style={{fontSize:78,fontFamily:'Times new roman'}}>Computer Enginerring</Card.Title>
